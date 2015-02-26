c1965fc5bf8e8718308e1041a88c2a591a447e6a
Milo Delgado <milo@nightout.com>
2015-02-26 08:14:59 -0800

    two column touch

--- wp-content/themes/blankstrap/two_column.php
+++ wp-content/themes/blankstrap/two_column.php
@@ -3,9 +3,7 @@
 Template Name: Two Column
 */
 ?>
-
 <?php get_template_part( 'templates/slider' ); ?>
-
 <div class="wrap container" role="document">
   <div class="content row">
     <div class="col-md-4">
